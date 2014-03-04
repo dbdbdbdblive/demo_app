37fb683b287a83b3389840f84528308a968e19db
db <dbdbdbdblive@gmail.com>
2014-03-03 16:55:26 -0800

    Initial commit. Just did rails new, and updated .gitignore per Listing1.7 in railstutorial.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DemoApp::Application.config.secret_key_base = '25739223e7be4fb43bfdd5f7d14d32e57578de0729b597bceca52d35eae8ac9b3c75949bb1ae67c10a6c96a441ad679adaef9300b3484a8f4a644dedb4d103c2'
